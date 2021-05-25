67743b6550eeacaab49feb5f5223a44d1177323a
Sairoden Gandarosa <72735313+Sairoden@users.noreply.github.com>
2021-05-25 15:38:54 +0800

    Delete config.js

--- src/utils/config.js
+++
@@ -1,9 +0,0 @@
-const GEOCODE =
-  "pk.eyJ1Ijoic2Fpcm9kZW4iLCJhIjoiY2tvcXE1ZWwzMDExYTJvbzVwdzFzMWF1cyJ9.lNOASVF98k7kUdU5jKBh2Q";
-
-const WEATHERSTACK = "cdbb6cfccc45a81432a7c0f0cd462e25";
-
-module.exports = {
-  GEOCODE,
-  WEATHERSTACK,
-};
